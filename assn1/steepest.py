f280173e24937a479f68a64c44051a72cd4ce49d
Ahn-Ssu <21600402@handong.edu>
2021-03-07 20:41:02 +0900

    출력 순서만 꼬였는데 대충 맞음

--- assn1/steepest.py
+++ assn1/steepest.py
@@ -1,8 +1,7 @@
-import math
 import sys
 
-def dist(x1,x2,y1,y2):
-    return math.abs( (x2-x1) / (y2-y1) )
+def steepness(x1,x2,y1,y2):
+    return abs(  (y2-y1)/ (x2-x1) )
 
 
 def merge_sort(target_list):
@@ -33,15 +32,37 @@ def merge(left, right):
             right = right[1:]
     return result
  
- 
- 
+def find_point(point_list):
+     
+    max_s = 0 
+    max_idx = 0
+
+    for i in range(len(point_list)-1):
+
+        now_s = steepness(point_list[i][0], point_list[i+1][0], point_list[i][1], point_list[i+1][1] )
+
+        if now_s > max_s :
+            max_s = now_s
+            max_idx = i
+
+
+    return max_idx
+
+
+ans_list= []
 stage = int(sys.stdin.readline())
-case = []
+
 
 for i in range(stage):
-    # case.append(sys.stdin.readline().split())
-    case.append(list(map(int, sys.stdin.readline().split())))
+    input_num = int(sys.stdin.readline())
+    case = []
+    for i in range(input_num):
+        
+        # case.append(sys.stdin.readline().split())
+        case.append(list(map(int, sys.stdin.readline().split())))
 
-sorted = merge_sort(case)
-print(sorted)
+    sorted = merge_sort(case)
 
+    final_idx = find_point(sorted)
+    ans = "{} {} {} {}".format(sorted[final_idx][0], sorted[final_idx][1], sorted[final_idx+1][0], sorted[final_idx+1][1])
+    ans_list.append(ans)
