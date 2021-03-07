bf4271aa3692c2bc77fcf8373d43ca72303f1706
Ahn-Ssu <21600402@handong.edu>
2021-03-07 22:00:22 +0900

    출력 방법 최적화 하고 quickSort로 바꿈

--- assn1/steepest.py
+++ assn1/steepest.py
@@ -4,34 +4,56 @@ def steepness(x1,x2,y1,y2):
     return abs(  (y2-y1)/ (x2-x1) )
 
 
-def merge_sort(target_list):
-    if len(target_list) <= 1:
-        return target_list
-    mid = len(target_list)//2
-    left = merge_sort(target_list[:mid])
-    right = merge_sort(target_list[mid:])
-    return merge(left,right)
+# def merge_sort(target_list):
+#     if len(target_list) <= 1:
+#         return target_list
+#     mid = len(target_list)//2
+#     left = merge_sort(target_list[:mid])
+#     right = merge_sort(target_list[mid:])
+#     return merge(left,right)
+
+# def merge(left, right):
+#     result = []
+#     while len(left) > 0 or len(right)>0:
+#         if len(left) > 0 and len(right) > 0:
  
-def merge(left, right):
-    result = []
-    while len(left) > 0 or len(right)>0:
-        if len(left) > 0 and len(right) > 0:
+#             if left[0] <= right[0]:
+#                 result.append(left[0])
+#                 left=left[1:]
+#             else:
+#                 result.append(right[0])
+#                 right = right[1:]
  
-            if left[0] <= right[0]:
-                result.append(left[0])
-                left=left[1:]
-            else:
-                result.append(right[0])
-                right = right[1:]
- 
-        elif len(left) > 0:
-            result.append(left[0])
-            left = left[1:]
-        elif len(right) > 0:
-            result.append(right[0])
-            right = right[1:]
-    return result
- 
+#         elif len(left) > 0:
+#             result.append(left[0])
+#             left = left[1:]
+#         elif len(right) > 0:
+#             result.append(right[0])
+#             right = right[1:]
+#     return result
+def quick_sort(arr):
+    def sort(low, high):
+        if high <= low:
+            return
+
+        mid = partition(low, high)
+        sort(low, mid - 1)
+        sort(mid, high)
+
+    def partition(low, high):
+        pivot = arr[(low + high) // 2]
+        while low <= high:
+            while arr[low] < pivot:
+                low += 1
+            while arr[high] > pivot:
+                high -= 1
+            if low <= high:
+                arr[low], arr[high] = arr[high], arr[low]
+                low, high = low + 1, high - 1
+        return low
+
+    return sort(0, len(arr) - 1)
+
 def find_point(point_list):
      
     max_s = 0 
@@ -61,11 +83,17 @@ for iter in range(stage):
         # case.append(list(map(int, sys.stdin.readline().split())))
         case[i] = list(map(int, sys.stdin.readline().split()))
 
-    sorted = merge_sort(case)
+    # sorted = merge_sort(case)
+    sorted = quick_sort(case)
+    # print(case)
 
-    final_idx = find_point(sorted)
-    ans = "{} {} {} {}".format(sorted[final_idx][0], sorted[final_idx][1], sorted[final_idx+1][0], sorted[final_idx+1][1])
+    final_idx = find_point(case)
+    # ans = "{} {} {} {}".format(sorted[final_idx][0], sorted[final_idx][1], sorted[final_idx+1][0], sorted[final_idx+1][1])
+    ans = "%s %s %s %s" % (case[final_idx][0], case[final_idx][1], case[final_idx+1][0], case[final_idx+1][1])
+    # f_str = "f'{" + '}{'.join([f'x{i}' for i in range(num_vars)]) + "}'"
     ans_list[iter] = ans
 
+output = ""
 for ans in ans_list:
-    print(ans)
+    output += ans +'\n'
+print(output)
