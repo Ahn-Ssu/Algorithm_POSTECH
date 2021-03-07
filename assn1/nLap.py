53950f196628661a356a6920d954e4bbd735d07d
Ahn-Ssu <21600402@handong.edu>
2021-03-07 12:53:22 +0900

    출력 재 조정

--- assn1/nLap.py
+++ assn1/nLap.py
@@ -1,39 +1,23 @@
 from collections import OrderedDict
 
-stage = input()
+stage = int(input())
+case = []
 
+for i in range(stage):
+    n_c = input()
+    seq = input().split()
+    case.append(seq)
 
-
-
-
-# print(list(OrderedDict.fromkeys(b)))
-# print(set(b))
-
-
-# for one in record:
-#     print(b)
-#     b.remove(one)
-#     print(b)
-#     print()
-
-
-# print(first[-1]) # ㅁㅏ지막꺼 출력 
 
 def lim (record_seq):
-    print("now record", record)
-
     if len(record_seq) == 0:
         print("NO")
         return 
     
     record.append(list(OrderedDict.fromkeys(record_seq)))
 
-    print("전",record_seq)
     for lap in record[-1]:
         record_seq.remove(lap)
-
-    print("후",record_seq)
-    print()
 
     if record[0] != record[-1]:
         print("YES")
@@ -42,8 +26,5 @@ def lim (record_seq):
     lim(record_seq)
 
 for i in range(stage):
-    
-    n, c = map(int, input().split())
-    s = input().split()
-    record = []
-    lim(b)
+    record =  []
+    lim(case[i])
