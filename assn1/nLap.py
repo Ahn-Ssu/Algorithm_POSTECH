aa45b0911a3726c2585e18f434f2e39058d23c33
Ahn-Ssu <21600402@handong.edu>
2021-03-07 17:17:56 +0900

    대칭 케이스 빼고 됨

--- assn1/nLap.py
+++ assn1/nLap.py
@@ -1,31 +1,48 @@
 import sys
-from collections import OrderedDict
+# from collections import OrderedDict
 
-stage = int(input())
-case = []
-
-for i in range(stage*2):
-    case.append(sys.stdin.readline())
+stage = int(sys.stdin.readline())
+case = ['2 3', '7 3 5 7 3 5', '2 2', '1 1 2 2 1 1', '3 4', '3 2 1 3 3 2 4 2 1 4 4 1', '3 5', '1 2 1 5 1 2 2 3 5 5 4 3 4 3 4', '4 4', '1 2 1 2 3 4 3 4 1 3 2 4 1 2 3 4']
 
 
-def lim (record_seq):
-    if len(record_seq) == 0:
-        print("NO")
-        return 
+def observ(record, N_car):
+
+  rank_recoding = {}
+  rank = []
+  lap_times = []
+
+  for idx in range(N_car):
+    rank.append(idx)
+    lap_times.append(0)
+  
+  for one_reco in record:
+
     
-    record.append(list(OrderedDict.fromkeys(record_seq)))
+    print(rank_recoding)
+    print(lap_times)
+    print(one_reco)
+    if one_reco in rank_recoding:
+      
+      for back in range(rank_recoding[one_reco]+1,N_car):
+        if lap_times[back] > lap_times[rank_recoding[one_reco]]:
+          print("YES")
+          return
+      lap_times[rank_recoding[one_reco]] += 1 
 
-    for lap in record[-1]:
-        record_seq.remove(lap)
 
-    if record[0] != record[-1]:
-        print("YES")
-        return
+    else:
+      rank_recoding[one_reco] = len(rank_recoding)
+      lap_times[rank_recoding[one_reco]] += 1 
+  
+  print("NO")
 
-    lim(record_seq)
+    
 
+for i in range(stage):
 
-print(case)
-# for i in range(stage):
-#     record =  []
-#     lim(case[i])
+  lapNcar = case[i*2].split()
+  a_record = case[i*2+1].split()
+
+  observ(a_record, int(lapNcar[1]))
+    
+
