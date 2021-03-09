9ef3f5ec35c4cbf7cb7b87de9b090a332739ceee
Ahn-Ssu <21600402@handong.edu>
2021-03-09 23:04:59 +0900

    Commit Test(Change repo name)

--- memo.py
+++ memo.py
@@ -1,16 +1,43 @@
-d1 = {}
-d2 = {} 
+# d1 = {}
+# d2 = {} 
 
 
-d1["7"] = 1
-d1["3"] = 2 
+# d1["7"] = 1
+# d1["3"] = 2 
 
-# d2["3"] = 5
-d2["5"] = 3 
-
-
-print(d1)
-print(d2)
+# # d2["3"] = 5
+# d2["7"] = 2
+# d2["3"] = 1 
 
 
-print("3" in d1)
+# print(d1)
+# print(d2)
+# print(d1==d2)
+# d1.update(d2)
+
+# print(d1)
+# print(d2)
+# print(d1==d2)
+
+
+# print(d1.values())
+# print("len", len(d1.values()))
+# print('7' in d1.keys())
+
+
+# print(5//2) # 2
+# print(6//2) # 3
+# print(4//2) # 2 
+
+# lists = [2,3,4,5,6,5]
+
+# print(len(lists))
+
+# print(lists[:2])
+# print(lists[2:])
+
+# lists = [0]*5
+# print(lists)
+
+for i in range(5,-1,-1):
+    print(i)
