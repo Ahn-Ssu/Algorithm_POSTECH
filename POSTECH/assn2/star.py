0f21d85aae5da904aff83ff1fb33ac06eb7d12d8
Ahn-Ssu <21600402@handong.edu>
2021-03-20 23:46:03 +0900

    created 4 star

---
+++ POSTECH/assn2/star.py
@@ -0,0 +1,39 @@
+import sys
+
+
+# stage = int(sys.stdin.readline())
+# ans_list= [0] * stage
+
+# for iter in range(stage):
+#     input_num = int(sys.stdin.readline())
+#     case = [0] * input_num
+#     for i in range(input_num):
+        
+#         case[i] = list(map(int, sys.stdin.readline().split()))
+
+
+#     # ans = "{} {} {} {}".format(sorted[final_idx][0], sorted[final_idx][1], sorted[final_idx+1][0], sorted[final_idx+1][1])
+#     # ans = "%s %s %s %s" % (case[final_idx][0], case[final_idx][1], case[final_idx+1][0], case[final_idx+1][1])
+#     # f_str = "f'{" + '}{'.join([f'x{i}' for i in range(num_vars)]) + "}'"
+#     # ans_list[iter] = ans
+
+# output = ""
+# for ans in ans_list:
+#     output += ans +'\n'
+# print(output)
+
+
+
+star_cage = ("*****", " * *", "  *")
+
+input_n = 3
+
+for iteration in range(input_n):
+    
+    
+
+
+
+
+
+
