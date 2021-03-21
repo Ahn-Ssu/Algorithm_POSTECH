1a2eaddb2cc74c2f577ef3c9dacbae4eb8848f34
Ahn-Ssu <21600402@handong.edu>
2021-03-21 15:18:46 +0900

    asdfasddfsa

--- POSTECH/assn2/MSS2.py
+++ POSTECH/assn2/MSS2.py
@@ -12,8 +12,52 @@ n = len(seq)
 def find_mss(target_seq, n):
 
     if n == 1 : 
-        return target_seq
+        return target_seq[0]
     elif n == 2 :
         local_max = target_seq[0]
 
-        for 
+        for one in target_seq:
+            if one > local_max:
+                local_max = one
+            
+        if target_seq[0]+target_seq[1] > local_max:
+            return target_seq[0]+target_seq[1]
+        else:
+            return local_max
+    
+    elif n == 3 :
+        local_max = target_seq[0]
+
+        for one in target_seq:
+            if one > local_max:
+                local_max = one
+        
+        for idx in range(len(n-1)):
+            if target_seq[idx]+target_seq[idx+1] > local_max:
+                local_max = target_seq[idx]+target_seq[idx+1]
+        
+        return local_max
+
+    else:
+
+        mid = n/2
+
+        leftMax = find_mss(target_seq[:mid], mid)
+        rigthMax = find_mss(target_seq[mid+1:], mid)
+        # center = find_mss(target_seq[mid:mid+1], 2)
+
+        if leftMax > rigthMax :
+            if target_seq[mid+1] > leftMax :
+                return find_mss(target_seq[mid:mid+1], 2)
+            elif target_seq[mid+1] > 0 :
+                return leftMax + target_seq[mid+1]
+            else:
+                return leftMax
+        
+        
+             
+        
+            
+        
+
+        
