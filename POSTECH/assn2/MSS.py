eb7eddc6947fc0ed8b9a5bd8e3c2c1e4018542eb
Ahn-Ssu <21600402@handong.edu>
2021-03-21 12:27:48 +0900

    한쪽은 됨

--- POSTECH/assn2/MSS.py
+++ POSTECH/assn2/MSS.py
@@ -1,6 +1,6 @@
 import sys
 import operator 
-# from functools import reduce
+from functools import reduce
 # seq = tuple(map(int,sys.stdin.readline().split()))
 
 # print(seq)
@@ -15,12 +15,59 @@ length = 1
 for idx in range(1,len(seq)-1,1):
 
     if max < (operator.add(seq[idx], seq[idx+1])):
-        print("now sub max value",seq[idx] + seq[idx+1])
         max = operator.add(seq[idx], seq[idx+1])
         max_idx = idx
-        print("now idx",idx)
+        length += 1 
+
+
+print()
+
+    
+def find_front(target_idx):
+    if operator.eq(target_idx, 0):
+        return -1
+    
+    if operator.gt(seq[target_idx], 0): # seq[target_idx] > 0
+        return target_idx
+    else:
+        return find_front(operator.add(target_idx, -1))
+        
+    
+
+
+front = max_idx -1
+back = max_idx + length
 
 
 
-for front in range(idx-1,0,-1):
-    print(front)
+while front >= 0 : 
+
+    if operator.gt(seq[front], 0 ): # seq
+        max_idx = operator.add(max_idx, -1)
+        length = operator.add(length, 1)
+        front = operator.add(front, -1)
+
+        # 더한 후, 다음 친구도 검사할거임 
+    else : # 0 보다 작은 경우 
+        #지금 위치로부터, 양수 위치 찾아 주는 함수  있으면 인덱스, 없으면 -1 
+        sub_idx = find_front(front)
+        print("sub_idx :",sub_idx)
+        print("now idx",max_idx)
+
+        if sub_idx:
+            print("sum",reduce(operator.add , seq[sub_idx:max_idx]))
+            if(reduce(operator.add , seq[sub_idx:max_idx]) > 0):
+                print("hi")
+                length += operator.sub(max_idx, sub_idx)
+                max_idx = sub_idx
+                front = max_idx -1
+            
+            else:
+                break
+        
+
+
+
+print("now idx",max_idx)
+print("now length",length)
+print("sum",reduce(operator.add , seq[max_idx:max_idx+length]))
